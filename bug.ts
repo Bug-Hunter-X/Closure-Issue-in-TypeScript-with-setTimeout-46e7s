3af1e4e5cb114d446ffed9b1d2de4d2c1cc527f5
BugHunter-X <askzenai@gmail.com>
2024-12-08 03:08:07 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,17 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); //This will work correctly
+
+function printNumbers1(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 0);
+  }
+}
+
+printNumbers1(5); //This will print 6 five times because of closure
